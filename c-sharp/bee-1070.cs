d202e65dcd0f9f44bd3ea69eedcd172658b76875
Heitor <hsznet72@gmail.com>
2022-03-07 18:51:10 -0300

    Adicionado exercício C#

---
+++ c-sharp/bee-1070.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+
+int y = x % 2;
+
+if (y == 0){
+    x = x + 1;
+}
+
+ int [] range = {1,2,3,4,5,6};
+
+foreach (int num in range)
+{
+    Console.WriteLine(x);
+    x = x +2;
+}
+    
